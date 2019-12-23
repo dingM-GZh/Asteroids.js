87ba5351bf4017cf36e29e5fb0b3f0dcb59194e3
Maxwell Ding <mbding@gmail.com>
2019-12-22 22:57:14 -0600

    Asteroids class completed. Need to implement spawning

--- asteroids.js
+++ asteroids.js
@@ -130,6 +130,51 @@ class Bullet {
 	}
 }
 
+class Asteroid {
+	constructor(x, y) {
+		this.visible = true;
+		this.x = Math.floor(Math.random() * canvasWidth); // rotating down the x-position of the asteroid
+		this.y = Math.floor(Math.random() * canvasHeight);
+		this.speed = 1;
+		this.radius = 50;
+		this.angle = Math.floor(Math.random() * 359);
+		this.strokeColor = 'white';
+	}
+
+	Update() {
+		var radians = this.angle / Math.PI * 180;
+		this.x += Math.cos(radians) * this.speed;
+		this.y += Math.sin(radians) * this.speed;
+
+		if (this.x < this.radius) {
+			this.x = canvas.width;
+		}
+		if (this.y < this.radius) {
+			this.y = canvas.height;
+		}
+		if (this.x > canvas.width) {
+			this.x = this.radius;
+		}
+		if (this.y > canvas.height) {
+			this.y = this.radius;
+		}
+	}
+
+	Draw() {
+		ctx.beginPath();
+		let vertAngle = ((Math.PI * 2) / 6);
+		var radians = this.angle / Math.PI * 180;
+
+		for (let i = 0; i < 6; i++) { // cycle through points of the triangle
+			ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians),
+				this.y - this.radius * Math.sin(vertAngle * i + radians));
+		}
+
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
+
 // draws and updates all positions of shapes on the screen
 function Render() {
 	ship.movingForward = (keys[87]); // 87 is w
